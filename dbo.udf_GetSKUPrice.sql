64f88dd75dcb0aabc17ca6467f7c963178ea776d
o-mikhneva <136434347+o-mikhneva@users.noreply.github.com>
2023-07-20 18:46:26 +0300

    3

--- dbo.udf_GetSKUPrice.sql
+++ dbo.udf_GetSKUPrice.sql
@@ -1,14 +1,7 @@
---Создать функцию (на выходе: файл в репозитории dbo.udf_GetSKUPrice.sql в ветке Functions)
---3.1 Входной параметр @ID_SKU
---3.2 Рассчитывает стоимость передаваемого продукта из таблицы dbo.Basket по формуле
---   3.1.1 сумма Value по переданному SKU / сумма Quantity по переданному SKU
---3.3 На выходе значение типа decimal(18, 2)
-
 if object_id('dbo.udf_GetSKUPrice') is not null
 	drop function dbo.udf_GetSKUPrice;
 go
 
--- Transact-SQL Scalar Function Syntax
 create function dbo.udf_GetSKUPrice
 ( 
 	@ID_SKU int
