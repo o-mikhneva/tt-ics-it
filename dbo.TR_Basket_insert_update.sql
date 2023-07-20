cc53e3884f5561cc3379a541cd65cd78f7f5e2e1
o-mikhneva <136434347+o-mikhneva@users.noreply.github.com>
2023-07-20 18:47:02 +0300

    6

--- dbo.TR_Basket_insert_update.sql
+++ dbo.TR_Basket_insert_update.sql
@@ -1,7 +1,3 @@
---Создать триггер (на выходе: файл в репозитории dbo.TR_Basket_insert_update в ветке Triggers)
---6.1 Если в таблицу dbo.Basket за раз добавляются 2 и более записей одного ID_SKU, то значение в поле DiscountValue 
---для этого ID_SKU рассчитывается по формуле Value * 5%, иначе DiscountValue = 0
-
 if  object_id('dbo.TR_Basket_insert_update', 'TR') is not null
 	drop trigger dbo.TR_Basket_insert_update; 
 go
