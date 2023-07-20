f1a8a8c405c92ebcf02641d14d0952391120a428
o-mikhneva <136434347+o-mikhneva@users.noreply.github.com>
2023-07-20 18:47:24 +0300

    4

--- dbo.vw_SKUPrice.sql
+++ dbo.vw_SKUPrice.sql
@@ -1,6 +1,3 @@
---Создать представление (на выходе: файл в репозитории dbo.vw_SKUPrice в ветке VIEWs)
---4.1 Возвращает все атрибуты продуктов из таблицы dbo.SKU и расчетный атрибут со стоимостью одного продукта (используя функцию dbo.udf_GetSKUPrice)
-
 if  object_id('dbo.vw_SKUPrice') is not null
 	drop view dbo.vw_SKUPrice; 
 go
