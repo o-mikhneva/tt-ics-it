6985cf98b4e8830bc9fba3ff34d8ca19cd9a37bf
o-mikhneva <136434347+o-mikhneva@users.noreply.github.com>
2023-07-21 18:33:15 +0300

    6

--- dbo.TR_Basket_insert_update.sql
+++ dbo.TR_Basket_insert_update.sql
@@ -9,8 +9,8 @@ as
 update dbo.Basket
 	set DiscountValue = Value * 0.05
 	where ID_SKU in (select ID_SKU
-					 from inserted
-					 group by ID_SKU
-					 having count(ID_SKU) >= 2);
+			 from inserted
+			 group by ID_SKU
+			 having count(ID_SKU) >= 2);
 
 go
